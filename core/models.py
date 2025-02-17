94c5511b450d0ffb527b03f043e38da46e41828e
AbduazizovaNozima <abduazizovanozima7@gmail.com>
2025-02-17 12:59:44 +0500

    id qoshdim

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db import models
 
 class Student(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     full_name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
     degree = models.CharField(max_length=200)
