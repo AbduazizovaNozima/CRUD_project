d2845bf27c4b557cb8686133c4d3aa6e9b8ac698
AbduazizovaNozima <abduazizovanozima7@gmail.com>
2025-02-19 12:16:05 +0500

    Restapi qoshildi

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('student/', views.student_view, name='student'),
+    path('api-auth/', include('rest_framework.urls'))
 ]
